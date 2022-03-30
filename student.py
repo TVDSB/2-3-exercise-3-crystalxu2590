48724fc2e95e79a58a0a28cab4e8b1ecb434f7d0
crystalxu2590 <100422327+crystalxu2590@users.noreply.github.com>
2022-03-29 22:36:17 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,5 +1,14 @@
 def main():
     #your code goes here
+    usersnumber = int(input("Please enter a number:\n"))
+    if usersnumber % 3 == 0 and usersnumber % 5 == 0:
+        print("fizzbuzz")
+    elif usersnumber % 3 == 0:
+        print("buzz")
+    elif usersnumber % 5 == 0:
+        print("buzz")
+    else:
+        print(usersnumber)
     print("0")
 
 if __name__ =='__main__':
